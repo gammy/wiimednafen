923a4a9d78154b11037e0873a207d94f968e9ec0
gammy <gammy@pean.org>
2014-02-23 23:00:11 +0100

    player.*

--- mednafen/src/player.cpp
+++ mednafen/src/player.cpp
@@ -22,16 +22,13 @@
 #include "video.h"
 #include "player.h"
 
-static UTF8 *AlbumName, *Artist, *Copyright, **SongNames;
+static std::string AlbumName, Artist, Copyright;
+static std::vector<std::string> SongNames;
 static int TotalSongs;
 
 static INLINE void DrawLine(MDFN_Surface *surface, uint32 color, uint32 bmatch, uint32 breplace, int x1, int y1, int x2, int y2)
 {
-#if PLAYER_BPP==16
- uint16 *buf = surface->pixels16;
-#else
  uint32 *buf = surface->pixels;
-#endif
  float dy_dx = (float)(y2 - y1) / (x2 - x1);
  int x;
 
@@ -90,7 +87,7 @@ static INLINE void DrawLine(MDFN_Surface *surface, uint32 color, uint32 bmatch, 
  }
 }
 
-int Player_Init(int tsongs, UTF8 *album, UTF8 *artist, UTF8 *copyright, UTF8 **snames)
+int Player_Init(int tsongs, const std::string &album, const std::string &artist, const std::string &copyright, const std::vector<std::string> &snames)
 {
  AlbumName = album;
  Artist = artist;
@@ -99,7 +96,6 @@ int Player_Init(int tsongs, UTF8 *album, UTF8 *artist, UTF8 *copyright, UTF8 **s
 
  TotalSongs = tsongs;
 
-#ifndef WII
  MDFNGameInfo->nominal_width = 384;
  MDFNGameInfo->nominal_height = 240;
 
@@ -108,21 +104,28 @@ int Player_Init(int tsongs, UTF8 *album, UTF8 *artist, UTF8 *copyright, UTF8 **s
 
  MDFNGameInfo->lcm_width = 384;
  MDFNGameInfo->lcm_height = 240;
-#endif
 
  MDFNGameInfo->GameType = GMT_PLAYER;
 
  return(1);
 }
 
+int Player_Init(int tsongs, const std::string &album, const std::string &artist, const std::string &copyright, char **snames)
+{
+ std::vector<std::string> tmpvec;
+
+ if(snames)
+ {
+  for(int i = 0; i < tsongs; i++)
+   tmpvec.push_back(snames[i] ? snames[i] : "");
+ }
+
+ return Player_Init(tsongs, album, artist, copyright, tmpvec);
+}
+
 void Player_Draw(MDFN_Surface *surface, MDFN_Rect *dr, int CurrentSong, int16 *samples, int32 sampcount)
 {
-#if PLAYER_BPP==16
- uint16 *XBuf = surface->pixels16;
-#else
  uint32 *XBuf = surface->pixels;
-#endif
-
  //MDFN_Rect *dr = &MDFNGameInfo->DisplayRect;
  int x,y;
  const uint32 text_color = surface->MakeColor(0xE8, 0xE8, 0xE8);
@@ -134,28 +137,18 @@ void Player_Draw(MDFN_Surface *surface, MDFN_Rect *dr, int CurrentSong, int16 *s
 
  dr->x = 0;
  dr->y = 0;
-#ifndef WII
  dr->w = 384;
  dr->h = 240;
-#else
- dr->w = MDFNGameInfo->nominal_width;
- dr->h = MDFNGameInfo->nominal_height;
-#endif
 
  // Draw the background color
  for(y = 0; y < dr->h; y++)
- {
-#if PLAYER_BPP==16
-      MDFN_FastU16MemsetM8(&XBuf[y * surface->pitch32], bg_color, dr->w);
-#else
-      MDFN_FastU32MemsetM8(&XBuf[y * surface->pitch32], bg_color, dr->w);
-#endif
- }
+  MDFN_FastU32MemsetM8(&XBuf[y * surface->pitch32], bg_color, dr->w);
 
  // Now we draw the waveform data.  It should be centered vertically, and extend the screen horizontally from left to right.
  int32 x_scale;
  float y_scale;
  int lastX, lastY;
+
 
  x_scale = (sampcount << 8) / dr->w;
 
@@ -179,7 +172,12 @@ void Player_Draw(MDFN_Surface *surface, MDFN_Rect *dr, int CurrentSong, int16 *s
     int ypos;
 
     ypos = (dr->h / 2) + (int)(y_scale * samp);
-    if(ypos < 0 || ypos >= dr->h) ypos = dr->h / 2;
+
+    if(ypos >= dr->h)
+     ypos = dr->h - 1;
+
+    if(ypos < 0)
+     ypos = 0;
 
     if(lastX >= 0) 
      DrawLine(surface, color, wc ? left_color : ~0, center_color, lastX, lastY, x, ypos);
@@ -189,42 +187,38 @@ void Player_Draw(MDFN_Surface *surface, MDFN_Rect *dr, int CurrentSong, int16 *s
   }
  }
 
-#if PLAYER_BPP==16
-int len = surface->pitch32 * sizeof(uint16);
-#else
-int len = surface->pitch32 * sizeof(uint32);
-#endif
-
  // Quick warning:  DrawTextTransShadow() has the possibility of drawing past the visible display area by 1 pixel on each axis.  This should only be a cosmetic issue
- // if 1-pixel line overflowing occurs onto the next line(most likely with NES, where width == pitch).  Fixme in the future?
+ // if 1-pixel line overflowing occurs onto the next line.  Fixme in the future?
+
  XBuf += 2 * surface->pitch32;
- if(AlbumName)
-  DrawTextTransShadow(XBuf, len, dr->w, AlbumName, text_color, text_shadow_color, 2);
+ DrawTextTransShadow(XBuf, surface->pitch32 * sizeof(uint32), dr->w, AlbumName, text_color, text_shadow_color, 1);
 
  XBuf += (13 + 2) * surface->pitch32;
- if(Artist)
-  DrawTextTransShadow(XBuf, len, dr->w, Artist, text_color, text_shadow_color, 2);
+ DrawTextTransShadow(XBuf, surface->pitch32 * sizeof(uint32), dr->w, Artist, text_color, text_shadow_color, 1);
 
  XBuf += (13 + 2) * surface->pitch32;
- if(Copyright)
-  DrawTextTransShadow(XBuf, len, dr->w, Copyright, text_color, text_shadow_color, 2);
+ DrawTextTransShadow(XBuf, surface->pitch32 * sizeof(uint32), dr->w, Copyright, text_color, text_shadow_color, 1);
 
  XBuf += (13 * 2) * surface->pitch32;
- 
+
  // If each song has an individual name, show this song's name.
- UTF8 *tmpsong = SongNames?SongNames[CurrentSong] : 0;
+ {
+  std::string tmpsong = "";
 
- if(!tmpsong && TotalSongs > 1)
-  tmpsong = (UTF8 *)_("Song:");
+  if((unsigned int)CurrentSong < SongNames.size())
+   tmpsong = SongNames[CurrentSong];
 
- if(tmpsong)
-  DrawTextTransShadow(XBuf, len, dr->w, tmpsong, text_color, text_shadow_color, 2);
- 
+  if(tmpsong == "" && TotalSongs > 1)
+   tmpsong = std::string(_("Song:"));
+
+  DrawTextTransShadow(XBuf, surface->pitch32 * sizeof(uint32), dr->w, tmpsong, text_color, text_shadow_color, 1);
+ }
+
  XBuf += (13 + 2) * surface->pitch32;
  if(TotalSongs > 1)
  {
   char snbuf[32];
   snprintf(snbuf, 32, "<%d/%d>", CurrentSong + 1, TotalSongs);
-  DrawTextTransShadow(XBuf, len, dr->w, (uint8*)snbuf, text_color, text_shadow_color, 2);
+  DrawTextTransShadow(XBuf, surface->pitch32 * sizeof(uint32), dr->w, (uint8*)snbuf, text_color, text_shadow_color, 1);
  }
 }
